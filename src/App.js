711a969a06ed48808f5ab1ccb037c7e92ec8e397
Jaideep1099 <jaideepkumar1099pm@gmail.com>
2022-03-22 16:42:41 +0530

    E2E Encryption added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import { getFirestore, collection, query, limit, orderBy, onSnapshot, addDoc, se
 import { doc, getDoc, getDocs, setDoc } from 'firebase/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
+const nacl = require('tweetnacl');
+const util = require('tweetnacl-util');
+
 const firebaseConfig = {
   // your config
   apiKey: "AIzaSyCWZEJsKrBPGRKJ0XSM7rq0f4lQnLEGJzQ",
@@ -20,7 +23,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore();
-
 const auth = getAuth();
 
 function App() {
@@ -28,22 +30,52 @@ function App() {
   const [user] = useAuthState(auth);
   const [regUser, setRegUser] = useState(false);  //User registered
 
+  const [secretKey, setSecretKey] = useState(null)
   console.log(user);
 
   return (
     <div className="App">
       <header>
         <h1>End-to-End 🔒 Encrypted Chat</h1>
-        <SignOut auth={auth} />
+        <SignOut auth={auth} setSecretKey={setSecretKey} setRegUser={setRegUser} />
       </header>
 
       <section>
-        {user ? (regUser ? <ChatRoom /> : <SignUp setRegUser={setRegUser} />)
+        {user ? (regUser ? (secretKey !== null ? <ChatRoom secretKey={secretKey} /> : <LoadKey setSecretKey={setSecretKey} />)
+          : <SignUp setRegUser={setRegUser} setSecretKey={setSecretKey} />)
           : <SignIn auth={auth} setRegUser={setRegUser} />}
       </section>
 
     </div>
   );
+}
+
+function LoadKey({ setSecretKey }) {
+  const [key, setKey] = useState(null)
+
+  const updateKey = (e) => {
+
+    let file = e.target.files[0]
+    const reader = new FileReader();
+    reader.readAsText(file)
+
+    reader.addEventListener("load", () => {
+      const keyObj = JSON.parse(reader.result);
+      const keyArr = util.decodeBase64(keyObj.key);
+      setKey({ string: keyObj.key, uint8: keyArr })
+    });
+
+  }
+
+  const useKey = () => {
+    setSecretKey({ ...key });
+  }
+  return (
+    <div className='fileUpload' style={{ backgroundColor: "white" }}>
+      <h1>Select Key file</h1><input type="file" name="keyf" accept='.json' onChange={updateKey} />
+      <button onClick={useKey}>Use Key</button>
+    </div>
+  )
 }
 
 function SignIn({ auth, setRegUser }) {
@@ -87,15 +119,20 @@ function SignIn({ auth, setRegUser }) {
 
 }
 
-function SignUp({ setRegUser }) {
+function SignUp({ setRegUser, setSecretKey }) {
 
   const generateKeys = () => {
-    return "dummykey";
+    const keypair = nacl.box.keyPair()
+
+    const publicKey = util.encodeBase64(keypair.publicKey);
+    const privateKey = util.encodeBase64(keypair.secretKey);
+    setSecretKey({ string: privateKey, uint8: keypair.secretKey });
+    return [privateKey, publicKey];
   }
 
   const regUser = async () => {
 
-    const pubKey = generateKeys();
+    const [pvtKey, pubKey] = generateKeys();
 
     await setDoc(doc(firestore, "users", auth.currentUser.uid), {
 
@@ -104,6 +141,18 @@ function SignUp({ setRegUser }) {
       photoURL: auth.currentUser.photoURL,
       pubKey: pubKey
     });
+
+    const key = { key: pvtKey };
+    const blob = new Blob([JSON.stringify(key)], { type: 'application/json' });
+
+    const a = document.createElement('a');
+    a.download = auth.currentUser.email + '_keyFile.json';
+    a.href = URL.createObjectURL(blob);
+
+    a.addEventListener('click', (e) => {
+      setTimeout(() => URL.revokeObjectURL(a.href), 30 * 1000);
+    });
+    a.click();
 
     setRegUser(true);
   }
@@ -115,10 +164,12 @@ function SignUp({ setRegUser }) {
   )
 }
 
-function SignOut({ auth }) {
+function SignOut({ auth, setSecretKey, setRegUser }) {
   const initiateSignOut = () => {
     signOut(auth).then(() => {
       console.log('Sign Out Success');
+      setSecretKey(null);
+      setRegUser(false);
     }).catch((error) => {
       console.log(error);
     })
@@ -129,8 +180,8 @@ function SignOut({ auth }) {
 }
 
 
-function ChatRoom() {
-  console.log('ChatRoom');
+function ChatRoom({ secretKey }) {
+  console.log('ChatRoom', secretKey);
   const [contacts, setContacts] = useState([]);
   const [selectedChat, setSelectedChat] = useState(null);
 
@@ -209,12 +260,14 @@ function ChatRoom() {
 
     </aside>
 
-    {selectedChat == null ? null : <Chat selectedChat={selectedChat} />}
+    {selectedChat == null ? null : <Chat selectedChat={selectedChat} secretKey={secretKey} />}
 
   </div>)
 }
 
-function Chat({selectedChat}) {
+function Chat({ selectedChat, secretKey }) {
+
+  console.log(selectedChat);
 
   const dummy = useRef();
   const [messages, setMessages] = useState([]);
@@ -236,15 +289,38 @@ function Chat({selectedChat}) {
       // dummy.current.scrollIntoView({ behavior: 'smooth' });
     });
 
-  }, [])
+  }, []);
+
+  const encryptMessage = (msg, pubKey) => {
+
+    console.log(msg, pubKey);
+    const pubKeyUint8Arr = util.decodeBase64(pubKey);
+    const msgUint8Arr = util.decodeUTF8(msg);
+    console.log(msgUint8Arr);
+    const nonce = nacl.randomBytes(nacl.box.nonceLength);
+    const encryptedMsg = nacl.box(msgUint8Arr, nonce, pubKeyUint8Arr, secretKey.uint8);
+    console.log(encryptedMsg);
+    console.log(util.encodeUTF8(nacl.box.open(
+      util.decodeBase64(util.encodeBase64(encryptedMsg)),
+      util.decodeBase64(util.encodeBase64(nonce)),
+      pubKeyUint8Arr,
+      secretKey.uint8
+    )));
+    return {
+      text: util.encodeBase64(encryptedMsg),
+      nonce: util.encodeBase64(nonce)
+    }
+  }
 
   const sendMessage = async (e) => {
     e.preventDefault();
 
     const { uid } = auth.currentUser;
 
+    const encryption = encryptMessage(formValue, selectedChat.pubKey)
+
     await addDoc(collection(firestore, selectedChat.chat_id), {
-      text: formValue,
+      ...encryption,
       createdAt: serverTimestamp(),
       uid
     });
@@ -255,8 +331,13 @@ function Chat({selectedChat}) {
 
   return (<>
     <main className="chat">
+      <h1>{selectedChat.displayName}</h1>
+      <h1>{selectedChat.displayName}</h1>
+      <h1>{selectedChat.displayName}</h1>
+      <h1 style={{color: "white"}}>{selectedChat.displayName}</h1>
 
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      {messages && messages.map(msg =>
+        <ChatMessage key={msg.id} message={msg} keys={{ pvt: secretKey, pub: selectedChat.pubKey }} />)}
 
       <span ref={dummy}></span>
 
@@ -273,14 +354,22 @@ function Chat({selectedChat}) {
 }
 
 function ChatMessage(props) {
-  const { text, uid, photoURL } = props.message;
+  const { text, uid, nonce } = props.message;
+  const { keys } = props;
+  console.log(keys);
+
+  const decryptedmsg = util.encodeUTF8(nacl.box.open(
+    util.decodeBase64(text),
+    util.decodeBase64(nonce),
+    util.decodeBase64(keys.pub),
+    keys.pvt.uint8
+  ));
 
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (<>
     <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
-      <p>{text}</p>
+      <p>{decryptedmsg}</p>
     </div>
   </>)
 }
